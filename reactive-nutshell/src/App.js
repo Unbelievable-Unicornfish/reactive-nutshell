6b87af95d4ea28fedd18b1248d1e7a7a20706e18
Emily Venturino <ebventurino@gmail.com>
2018-07-17 15:55:34 -0500

    added chat button

--- reactive-nutshell/src/App.js
+++ reactive-nutshell/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from "./Login"
+import ChatList from "./ChatList"
 
 class App extends Component {
   render() {
     return (
-     <Login />
+     <ChatList />
     );
   }
 }
