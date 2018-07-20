02d512ee617d7d8486c783b35ad1281ca83822fc
Lauren Richert <lsrichert@gmail.com>
2018-07-20 11:22:25 -0500

    fixing more merge conflicts

--- reactive-nutshell/src/App.js
+++ reactive-nutshell/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from "react"
 import './App.css';
-import ArticleList from './Articles/ArticleList';
+import ArticleList from './Articles/ArticleList'
 import Login from "./Login"
 import ChatList from "./Chat/ChatList"
-
 import TaskList from "./Task/TaskList"
-<<<<<<< HEAD
 import EventList from "./Event/EventList"
-=======
-
-
-
-
->>>>>>> master
 
 class App extends Component {
   render() {
     return (
     
-<<<<<<< HEAD
       <React.Fragment>
      <Login />
      <ChatList />
@@ -26,15 +17,6 @@ class App extends Component {
      <EventList />
      <ArticleList />
      </React.Fragment>
-=======
-
-    <React.Fragment>
-      <Login />
-      <ArticleList />
-      <ChatList />
-      <TaskList />
-    </React.Fragment>
->>>>>>> master
     )
   }
 }
